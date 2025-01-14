58170a4edb7f9729208e28792a66e68aff634dd7
Joel-Aviles <j_aviles.b@seg.guanajuato.gob.mx>
2025-01-14 10:02:12 -0600

    refactor: :recycle: se agrego una validacion para usar identificar que url usar dependiendo del amiente

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = import.meta.env.VITE_BASE_URL;
+const ENVARIOMENT = "prod"
+export const API_BASE_URL = ENVARIOMENT === "prod" ? import.meta.env.VITE_PROD_BASE_URL : import.meta.env.VITE_BASE_URL;
 export const FORM_FIELD_MAPPING = {
   tipoProceso: "process_type",
   porcentaje: "discount_percent",
